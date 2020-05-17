b612a3cc89f361ff0c9ded91fe4cfe3eec124041
Adam Miartus <adam.miartus@gmail.com>
2020-05-17 18:01:42 +0200

    initial
    Signed-off-by: Adam Miartus <adam.miartus@gmail.com>

---
+++ js/time.js
@@ -0,0 +1,58 @@
+/* For a given date, get the ISO week number
+ *
+ * Based on information at:
+ *
+ *    http://www.merlyn.demon.co.uk/weekcalc.htm#WNR
+ *
+ * Algorithm is to find nearest thursday, it's year
+ * is the year of the week number. Then get weeks
+ * between that date and the first day of that year.
+ *
+ * Note that dates in one year can be weeks of previous
+ * or next year, overlap is up to 3 days.
+ *
+ * e.g. 2014/12/29 is Monday in week  1 of 2015
+ *      2012/1/1   is Sunday in week 52 of 2011
+ */
+export function getWeekNumber(d) {
+	// Copy date so don't modify original
+	d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+	// Set to nearest Thursday: current date + 4 - current day number
+	// Make Sunday's day number 7
+	d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+	// Get first day of year
+	var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+	// Calculate full weeks to nearest Thursday
+	var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+	// Return array of year and week number
+	return [d.getUTCFullYear(), weekNo];
+}
+
+export function convertMS(milliseconds) {
+	var day, hour, minute, seconds;
+	seconds = Math.floor(milliseconds / 1000);
+	minute = Math.floor(seconds / 60);
+	seconds = seconds % 60;
+	hour = Math.floor(minute / 60);
+	minute = minute % 60;
+	day = Math.floor(hour / 24);
+	hour = hour % 24;
+	return {
+		day: day,
+		hour: hour,
+		minute: minute,
+		seconds: seconds
+	};
+}
+
+export function toDuration(msec) {
+	var sec_num = parseInt(msec, 10); // don't forget the second param
+	var hours   = Math.floor(sec_num / 3600);
+	var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+	var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+	if (hours   < 10) {hours   = "0"+hours;}
+	if (minutes < 10) {minutes = "0"+minutes;}
+	if (seconds < 10) {seconds = "0"+seconds;}
+	return hours+':'+minutes+':'+seconds;
+}
